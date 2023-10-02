ff1625af7aa7aa9cf9321a6d5e66ad798587ad97
lcaldera <lasantha.caldera@dualinventive.com>
2023-10-02 13:44:57 +0530

    added .env

--- tests/sample.spec.ts
+++ tests/sample.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
 
 test("test 1", async ({ page }) => {
-  await page.goto("/");
-  expect(await page.title()).toEqual("GitHub: Let’s build from here · GitHub");
+  await page.goto("/api-mocking");
+  expect(await page.title()).toEqual("Playwright API Mocking demo");
 });
